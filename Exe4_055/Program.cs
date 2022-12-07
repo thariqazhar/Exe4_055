7457d23f67df739ecc0f986e0f2403313168b14b
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2022-12-07 16:37:25 +0700

    Creating a method for stack

--- Exe4_055/Program.cs
+++ Exe4_055/Program.cs
@@ -7,5 +7,13 @@ namespace Exception
         int[] azhar = new int[3];
         private int top;
         private int max;
+
+        public Stack(int size)
+        {
+            azhar = new int[size];
+            top = -1;
+            max = size;
+        }
+
     }
 }
