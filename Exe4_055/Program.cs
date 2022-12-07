3696b6abc5eef613e1e5a74daae69aa8303055a3
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2022-12-07 16:44:55 +0700

    Making a case for push

--- Exe4_055/Program.cs
+++ Exe4_055/Program.cs
@@ -79,6 +79,15 @@ namespace Exception
                 Console.WriteLine("\nEnter your choice: ");
                 string sInput = Console.ReadLine();
                 char ch = Convert.ToChar(sInput == "" ? "0" : sInput);
+
+                switch (ch)
+                {
+                    case '1':
+                        Console.Write("\nEnter a number: ");
+                        int num = Convert.ToInt32(Console.ReadLine());
+                        s.push(num);
+                        break;
+                }
             }
         }
     }
